080d8ddc3650346286d9c050f8cc4d58664768ae
hajush <harold@shinsato.com>
2017-03-21 13:03:14 -0600

    Make sure npm test does both unit tests and lint. Add models to what is linted.

--- models/userSchema.js
+++ models/userSchema.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 let UserSchema = new mongoose.Schema({
   name: String,
   bio: String,
-  neighborhood: String,
+  neighborhood: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
