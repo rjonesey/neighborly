808f75cc00b0e811bfd9cd998f63470695270be4
Ricky Jones <richard.jones441@gmail.com>
2017-04-05 12:32:18 -0600

    updated version and request button fix

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -24,7 +24,6 @@ function ItemList(props) {
           <CardSubtitle>{item.condition}</CardSubtitle>
           <CardText>{item.description}</CardText>
           <CardText>{addedBy}</CardText>
-          <Button>Request</Button>
           <LinkContainer to={{pathname: '/Requested'}}>
             <Button>Request</Button>
           </LinkContainer>
